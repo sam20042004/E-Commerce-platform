9ae79b8dfc3406cd7d763d3d3dc239df2e30f2fa
sam20042004 <jhandasamyak1@gmail.com>
2024-08-13 12:47:06 +0530

    made the user backend, now going to build frontend

---
+++ backend/middlewares/asyncHandler.js
@@ -0,0 +1,7 @@
+const asynchHandler = (fn) => (req,res,next)=>{
+    Promise.resolve(fn(req,res,next)).catch(error => {
+        res.status(500).json({message:error.message});
+    })
+}
+
+export default asynchHandler;
---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// This is the schema of the model.
+const userSchema = mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type: String,
+            required:true,
+            unique:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false,  
+        }
+    },
+    {timestamps:true}
+);
+
+// This is the model of user
+const User = mongoose.model('User',userSchema)
+
+export default User;
