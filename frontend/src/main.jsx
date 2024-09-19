14fef1b0f08db09744b35d7d00248cd5284e61d2
sam20042004 <jhandasamyak1@gmail.com>
2024-09-19 16:16:45 +0530

    fg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,6 +28,9 @@ import Cart from "./pages/Cart.jsx";
 import Shop from "./pages/shop.jsx";
 import Shipping from "./pages/Orders/Shipping.jsx";
 import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
+import Order from "./pages/Orders/Order.jsx"
+
+import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -46,6 +49,8 @@ const router = createBrowserRouter(
         <Route path="profile" element={<Profile />} />
         <Route path="shipping" element={<Shipping />} />
         <Route path="placeorder" element={<PlaceOrder />} />
+        <Route path="order/:id" element={<Order />} />
+
       </Route>
 
       {/* Admin Routes */}
@@ -63,6 +68,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
   </Provider>
 );
--- frontend/src/pages/Orders/Order.jsx
+++ frontend/src/pages/Orders/Order.jsx
@@ -1,120 +1,230 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Messsage from "../../components/Message";
+import Loader from "../../components/Loader";
 import {
-  saveShippingAddress,
-  savePaymentMethod,
-} from "../../redux/features/cart/cartSlice";
-import ProgressSteps from "../../components/ProgressSteps";
+  useDeliverOrderMutation,
+  useGetOrderDetailsQuery,
+  useGetPaypalClientIdQuery,
+  usePayOrderMutation,
+} from "../../redux/api/orderApiSlice";
 
-const Shipping = () => {
-  const cart = useSelector((state) => state.cart);
-  const { shippingAddress } = cart;
+const Order = () => {
+  const { id: orderId } = useParams();
 
-  const [paymentMethod, setPaymentMethod] = useState("PayPal");
-  const [address, setAddress] = useState(shippingAddress.address || "");
-  const [city, setCity] = useState(shippingAddress.city || "");
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress.postalCode || ""
-  );
-  const [country, setCountry] = useState(shippingAddress.country || "");
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+  const [deliverOrder, { isLoading: loadingDeliver }] =
+    useDeliverOrderMutation();
+  const { userInfo } = useSelector((state) => state.auth);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
-    dispatch(saveShippingAddress({ address, city, postalCode, country }));
-    dispatch(savePaymentMethod(paymentMethod));
-    navigate("/placeorder");
+  const {
+    data: paypal,
+    isLoading: loadingPaPal,
+    error: errorPayPal,
+  } = useGetPaypalClientIdQuery();
+
+  useEffect(() => {
+    if (!errorPayPal && !loadingPaPal && paypal.clientId) {
+      const loadingPaPalScript = async () => {
+        paypalDispatch({
+          type: "resetOptions",
+          value: {
+            "client-id": paypal.clientId,
+            currency: "USD",
+          },
+        });
+        paypalDispatch({ type: "setLoadingStatus", value: "pending" });
+      };
+
+      if (order && !order.isPaid) {
+        if (!window.paypal) {
+          loadingPaPalScript();
+        }
+      }
+    }
+  }, [errorPayPal, loadingPaPal, order, paypal, paypalDispatch]);
+
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        await payOrder({ orderId, details });
+        refetch();
+        toast.success("Order is paid");
+      } catch (error) {
+        toast.error(error?.data?.message || error.message);
+      }
+    });
+  }
+
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [{ amount: { value: order.totalPrice } }],
+      })
+      .then((orderID) => {
+        return orderID;
+      });
+  }
+
+  function onError(err) {
+    toast.error(err.message);
+  }
+
+  const deliverHandler = async () => {
+    await deliverOrder(orderId);
+    refetch();
   };
 
-  // Payment
-  useEffect(() => {
-    if (!shippingAddress.address) {
-      navigate("/shipping");
-    }
-  }, [navigate, shippingAddress]);
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Messsage variant="danger">{error.data.message}</Messsage>
+  ) : (
+    <div className="container flex flex-col ml-[10rem] md:flex-row">
+      <div className="md:w-2/3 pr-4">
+        <div className="border gray-300 mt-5 pb-4 mb-5">
+          {order.orderItems.length === 0 ? (
+            <Messsage>Order is empty</Messsage>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="w-[80%]">
+                <thead className="border-b-2">
+                  <tr>
+                    <th className="p-2">Image</th>
+                    <th className="p-2">Product</th>
+                    <th className="p-2 text-center">Quantity</th>
+                    <th className="p-2">Unit Price</th>
+                    <th className="p-2">Total</th>
+                  </tr>
+                </thead>
 
-  return (
-    <div className="container mx-auto mt-10">
-      <ProgressSteps step1 step2 />
-      <div className="mt-[10rem] flex justify-around items-center flex-wrap">
-        <form onSubmit={submitHandler} className="w-[40rem]">
-          <h1 className="text-2xl font-semibold mb-4">Shipping</h1>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Address</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter address"
-              value={address}
-              required
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">City</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter city"
-              value={city}
-              required
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Postal Code</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter postal code"
-              value={postalCode}
-              required
-              onChange={(e) => setPostalCode(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Country</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded"
-              placeholder="Enter country"
-              value={country}
-              required
-              onChange={(e) => setCountry(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-400">Select Method</label>
-            <div className="mt-2">
-              <label className="inline-flex items-center">
-                <input
-                  type="radio"
-                  className="form-radio text-pink-500"
-                  name="paymentMethod"
-                  value="PayPal"
-                  checked={paymentMethod === "PayPal"}
-                  onChange={(e) => setPaymentMethod(e.target.value)}
-                />
+                <tbody>
+                  {order.orderItems.map((item, index) => (
+                    <tr key={index}>
+                      <td className="p-2">
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="w-16 h-16 object-cover"
+                        />
+                      </td>
 
-                <span className="ml-2">PayPal or Credit Card</span>
-              </label>
+                      <td className="p-2">
+                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                      </td>
+
+                      <td className="p-2 text-center">{item.qty}</td>
+                      <td className="p-2 text-center">{item.price}</td>
+                      <td className="p-2 text-center">
+                        $ {(item.qty * item.price).toFixed(2)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-          </div>
+          )}
+        </div>
+      </div>
 
-          <button
-            className="bg-pink-500 text-white py-2 px-4 rounded-full text-lg w-full"
-            type="submit"
-          >
-            Continue
-          </button>
-        </form>
+      <div className="md:w-1/3">
+        <div className="mt-5 border-gray-300 pb-4 mb-4">
+          <h2 className="text-xl font-bold mb-2">Shipping</h2>
+          <p className="mb-4 mt-4">
+            <strong className="text-pink-500">Order:</strong> {order._id}
+          </p>
+
+          <p className="mb-4">
+            <strong className="text-pink-500">Name:</strong>{" "}
+            {order.user.username}
+          </p>
+
+          <p className="mb-4">
+            <strong className="text-pink-500">Email:</strong> {order.user.email}
+          </p>
+
+          <p className="mb-4">
+            <strong className="text-pink-500">Address:</strong>{" "}
+            {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+            {order.shippingAddress.postalCode}, {order.shippingAddress.country}
+          </p>
+
+          <p className="mb-4">
+            <strong className="text-pink-500">Method:</strong>{" "}
+            {order.paymentMethod}
+          </p>
+
+          {order.isPaid ? (
+            <Messsage variant="success">Paid on {order.paidAt}</Messsage>
+          ) : (
+            <Messsage variant="danger">Not paid</Messsage>
+          )}
+        </div>
+
+        <h2 className="text-xl font-bold mb-2 mt-[3rem]">Order Summary</h2>
+        <div className="flex justify-between mb-2">
+          <span>Items</span>
+          <span>$ {order.itemsPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Shipping</span>
+          <span>$ {order.shippingPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Tax</span>
+          <span>$ {order.taxPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Total</span>
+          <span>$ {order.totalPrice}</span>
+        </div>
+
+        {!order.isPaid && (
+          <div>
+            {loadingPay && <Loader />}{" "}
+            {isPending ? (
+              <Loader />
+            ) : (
+              <div>
+                <div>
+                  <PayPalButtons
+                    createOrder={createOrder}
+                    onApprove={onApprove}
+                    onError={onError}
+                  ></PayPalButtons>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+
+        {loadingDeliver && <Loader />}
+        {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+          <div>
+            <button
+              type="button"
+              className="bg-pink-500 text-white w-full py-2"
+              onClick={deliverHandler}
+            >
+              Mark As Delivered
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default Shipping;
+export default Order;
